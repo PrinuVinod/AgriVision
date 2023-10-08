7a298ebeb92702726cc4de4f67545bd9565d46c6
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-10-09 01:07:49 +0530

    contact us form added

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycby16_-vfX_xHjFyyIlfIlUf1IQa7G003SZFOjE4506yWPeo4EcIBa_DVxZBBXPr59XUMQ/exec'
+			const form = document.forms['contactForm']
+		  
+			form.addEventListener('submit', e => {
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+				.then(response => alert("Thank you! Prinu will contact you soon." ))
+				.catch(error => console.error('Error!', error.message))
+			})
