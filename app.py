0d6bd844e1905380774f451c1c06871520355f0a
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2024-02-07 13:02:09 +0530

    detection works

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import subprocess
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
 
@@ -6,10 +8,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
 @app.route('/detection')
 def detection():
     return render_template('detection.html')
+
+@app.route('/start_detection')
+def start_detection():
+    try:
+        # Use subprocess to run the detect.py file
+        subprocess.run(['python', 'detect.py'], check=True)
+        return "Detection started!"
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e}"
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
